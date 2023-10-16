724f408c254732d69eca5a0a63822853cb920a7f
xuebusi <xuebusi@qq.com>
2023-10-16 19:33:29 +0800

    4.玩家射击

--- Asteroids/Constants.swift
+++ Asteroids/Constants.swift
@@ -14,3 +14,15 @@ func deg2rad(degrees: Double) -> Double {
 func rad2deg(radians: Double) -> Double {
     radians * 180 / .pi
 }
+
+func findDestination(start: CGPoint, distance: CGFloat = 1152, angle: CGFloat) -> CGPoint {
+    var x: Double = 0
+    var y: Double = 0
+    var angleInRadians: Double = 0
+    
+    angleInRadians = deg2rad(degrees: angle + 90)
+    x = distance * cos(angleInRadians)
+    y = distance * sin(angleInRadians)
+    
+    return CGPoint(x: start.x + x, y: start.y + y)
+}
--- Asteroids/Scenes/Game Scene/GameScene.swift
+++ Asteroids/Scenes/Game Scene/GameScene.swift
@@ -86,6 +86,8 @@ class GameScene: SKScene {
             scene?.run(thrustSound, withKey: "thrustSound")
         case "hyper":
             animateHyperSpace()
+        case "fire":
+            createPlayerBullet()
         default:
             return
         }
@@ -155,5 +157,25 @@ class GameScene: SKScene {
         let wait = SKAction.wait(forDuration: 0.25)
         let animation = SKAction.sequence([stopShooting, outAnimation, wait, movePlayer, wait, inAnimation, startShooting])
         player.run(animation)
+    }
+    
+    func createPlayerBullet() {
+        guard isHyperSpacingOn == false && isPlayerAlive == true else { return }
+        
+        let bullet = SKShapeNode(ellipseOf: CGSize(width: 3, height: 3))
+        let shotSound = SKAction.playSoundFileNamed("fire.wav", waitForCompletion: false)
+        let move = SKAction.move(to: findDestination(start: player.position, angle: rotation), duration: 0.5)
+        let sequence = SKAction.sequence([shotSound, move, .removeFromParent()])
+        
+        bullet.position = player.position
+        bullet.zPosition = 0
+        bullet.fillColor = .white
+        bullet.name = "playerBullet"
+        addChild(bullet)
+        
+        bullet.physicsBody = SKPhysicsBody(circleOfRadius: 3)
+        bullet.physicsBody?.affectedByGravity = false
+        bullet.physicsBody?.isDynamic = true
+        bullet.run(sequence)
     }
 }
